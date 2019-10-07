eda867f391562773e49e1ab200106e40b83a7e4f
Benjamin Green <ben@Benjamins-MacBook-Pro.local>
2019-10-07 19:28:33 +1100

    updates

--- models/item.py
+++ models/item.py
@@ -16,11 +16,19 @@ class ItemModel(db.Model):
         self.store_id = store_id
 
     def json(self):
-        return {'name': self.name, 'price': self.price}
+        return {
+            'name': self.name,
+            'price': self.price,
+            'store_id': self.store_id
+        }
 
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_all():
+        return cls.query.all()
 
     def save_to_db(self):
         db.session.add(self)
